c71a61273daecae575a6805c1b4b8a51e8099338
Simen Mailund Svendsen <simen@erus.no>
2024-05-15 08:40:57 +0200

    surface errors in timestamp conversion

--- src/blocks/weather/open_weather_map.rs
+++ src/blocks/weather/open_weather_map.rs
@@ -388,8 +388,15 @@ impl WeatherProvider for Service<'_> {
             })
         };
 
-        let sunrise = DateTime::<Utc>::from_timestamp(current_data.sys.sunrise, 0);
-        let sunset = DateTime::<Utc>::from_timestamp(current_data.sys.sunset, 0);
+        let sunrise = Some(
+            DateTime::<Utc>::from_timestamp(current_data.sys.sunrise, 0)
+                .error("Unable to convert timestamp to DateTime")?,
+        );
+
+        let sunset = Some(
+            DateTime::<Utc>::from_timestamp(current_data.sys.sunset, 0)
+                .error("Unable to convert timestamp to DateTime")?,
+        );
 
         Ok(WeatherResult {
             location: current_data.name,
